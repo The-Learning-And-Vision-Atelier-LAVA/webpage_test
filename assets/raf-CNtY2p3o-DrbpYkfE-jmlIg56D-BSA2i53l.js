de20c46e0274ad67890eebda4568fcf2f802dedf
kunbpc <498031406@qq.com>
2024-12-29 19:22:30 +0800

    modify the base dir
    Signed-off-by: kunbpc <498031406@qq.com>

---
+++ assets/raf-CNtY2p3o-DrbpYkfE-jmlIg56D-BSA2i53l.js
@@ -0,0 +1,1 @@
+import{aF as i}from"./index-BrdBUvI4.js";const m=e=>i?window.requestAnimationFrame(e):setTimeout(e,16),a=e=>i?window.cancelAnimationFrame(e):clearTimeout(e);export{m as i,a as m};
--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ import Unocss from 'unocss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: './',
+  base: 'webpage_test/',
   resolve: {
     alias: {
       '~/': `${path.resolve(__dirname, 'src')}/`,
